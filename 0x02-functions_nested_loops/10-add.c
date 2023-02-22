ce699e6ce9b3cb7dd0226617e67f60bf258d4fc1
Warona <thatowarona7@gmail.com>
2023-02-21 16:58:43 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -Adds two values
+ * @n1: The first number to br computed
+ * @n2: The second nuumber to be computed
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
