26a8a8b75e9873c145945b6fdda2330f24459f6e
Warona <thatowarona7@gmail.com>
2023-02-27 12:54:02 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -25,13 +25,12 @@ int main(void)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
-
+		
 		if ((sum - 2772) % 2 != 0)
-				diff_half1++;
-
-				for (index = 0; password[index]; index++)
-				{
-					if (password[index] >= (33 + diff_half1))
+			diff_half1++;
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
 				{
 					password[index] -= diff_half1;
 
