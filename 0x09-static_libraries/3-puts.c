e76e6de786366f6e24c588a92f3bd582f58120a0
Warona <thatowarona7@gmail.com>
2023-03-09 22:33:53 -0800

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str);
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like biulding a multilingual puzzle");
+	return (0);
+}
